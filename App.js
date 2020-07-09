d07cd6e5bec2c1727f7c1dd799cb90d55854aa47
Nguyen Duc Hanh <1851061469@e.tlu.edu.vn>
2020-07-09 23:50:09 +0700

    fix header

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, SafeAreaView } from "react-native";
 
 import Header from "./components/Header/Header";
 import Total from "./components/Total/Total";
@@ -10,13 +10,13 @@ import Nav from "./components/Nav";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Icon style={styles.icon} />
       <Header style={styles.header} />
       <Total style={styles.statistic} />
       <Gallery style={styles.gallery} />
       <Nav style={styles.nav} />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "column",
   },
-  icon: { flex: 0.1 },
+  icon: { flex: 0.1, marginTop: 20 },
   header: {
     flex: 0.2,
   },
